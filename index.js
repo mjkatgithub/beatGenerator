ac34fd5eb3979a54d813cd3d733e6a1f2aee4c19
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2019-06-07 08:46:24 +0200

    add perc generator

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const scribble = require('scribbletune');
 
 function generateMelo (){
-  let melo = scribble.clip({
-    notes: 'c4',
-    pattern: 'x'
+  let melo1 = scribble.clip({
+    //notes: ['c4', 'd4', 'e4', 'f4', 'g4', 'b4', 'c5'],
+    notes: scribble.scale('c minor')
+            .filter((a, x) => x % 2 === 0),
+    pattern: 'x-x-x-x-x-x-x-x-'
   });
-  scribble.midi(melo, 'melo.midi');
+
+  //console.log(melo1);
+
+  let melo2 = scribble.clip({
+    //notes: ['c4', 'd4', 'e4', 'f4', 'g4', 'b4', 'c5'],
+    notes: scribble.scale('c minor')
+            .filter((a, x) => x % 2),
+    pattern: 'x-'.repeat(8)
+  });
+
+  scribble.midi(melo1.concat(melo2), 'melo.midi');
 }
 
+
+function generatePerc() {
+  let perc1 = scribble.clip({
+    notes: ['c2', 'c#2', 'd2', 'd#2'],
+    pattern: 'x-x-x--x',
+    sizzle: true,
+    shuffle: true
+  });
+
+  let perc2 = scribble.clip({
+    notes: ['c2', 'c#2', 'd2', 'd#2'],
+    pattern: 'x-x-xxxx',
+    sizzle: true,
+    shuffle: true
+  });
+
+  scribble.midi(perc1.concat(perc1, perc1, perc2), 'perc.midi');
+}
+
+
 generateMelo();
+generatePerc();
